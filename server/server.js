a7c2bb4ebe6d0f9af01276cd55daf06732d273bb
Harry P <harryepotter@outlook.com>
2024-05-07 19:57:13 +1000

    added images

--- server/server.js
+++ server/server.js
@@ -20,6 +20,8 @@ const startApolloServer = async () => {
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
+  app.use(express.static(path.join(__dirname, "../client/public")));
+  
   app.use(
     "/graphql",
     expressMiddleware(server, {
